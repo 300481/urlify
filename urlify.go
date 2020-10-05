6700d5b298332f22838b8baf024959b2902b7790
300481 <300481@300481.de>
2020-10-05 17:38:21 +0200

    Use Environment Variable for the URL to fetch as proxy

--- urlify.go
+++ urlify.go
@@ -4,11 +4,13 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 // Handle will handle the cloud function call
 func Handle(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("https://media.os.fressnapf.com/cms/2020/08/5f2cfc5f7f982-5f2cfc5f7f983Ratgeber-Katze_Outdoor_Kitten_1200x527.jpg.jpg?t=cmsimg_920")
+	url := os.Getenv("URL")
+	resp, err := http.Get(url)
 
 	if err != nil {
 		log.Println("some error when fetching data")
